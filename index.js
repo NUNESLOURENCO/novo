f4cd23edc1105786b66573c6d053fcf8de1a7039
Anderson Lourenço <andersonuzn@gmail.com>
2019-11-29 22:11:28 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+console.log('sentença de codicos');
+console.log('Olá');
+console.log('Ander')
+// Padrão do curso
+
+//tipos de comentários aula 2
+console.log('linhas')
+
+/* comentários de múltiplas linhas*/
+console.log('linhas 2')
+
+/*
+*comentários de
+*múltiplas
+* linhas
+*/
+console.log(" tn é um filha da puta do caralho nunca mais quero ver ele de novo seu arrombado.")
+
+console.log("linha 3")
+
+//aula 3 (2 palavras resevadas  let e const.)
+
+
+let a = 3
+let b = 4
+
+
+   a = 30
+   b = 40
+  
+ console.log(a, b)
+ a=300
+ b=400
+ console.log(a, b)
+ const c=5
+ //c=50
+ console.log(c
+ )
+  // aula 4.
+  let qualquer = 'Legal'
+  console.log(qualquer)
+  console.log(typeof qualquer)
+  
+  qualquer= 3.1516
+  console.log(qualquer)
+  console.log(typeof qualquer)
+  //Eiter nomes genéricos e siglas 
+ // exemplos de não fazer
+ let valor= ""
+ let número=1
+ let pqp=false// produto  químico perigoso.....
+ 
+ //aula 5 tipos números. js
+ 
+ const peso1 = 1.0
+ const peso2= Number('2.0')
+  
+  console.log( peso1, peso2)
+ console.log(Number. isInteger(peso1))
